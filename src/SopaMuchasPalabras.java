84cd97f46f1bd397b2a623649a66215a0636d62b
Your Name <you@example.com>
2016-05-21 18:25:03 +0200

    Clase SopaMuchasMalabras Signed-off-by: Celeste<you@example.com>

---
+++ src/SopaMuchasPalabras.java
@@ -0,0 +1,27 @@
+
+public class SopaMuchasPalabras {
+/*
+ * Constructor de la clase
+ */
+	public SopaMuchasPalabras() {}
+/*
+ * Método de palabras que faltan por buscar
+ */
+	 public String buscar (){}
+/*
+ * Método que entrelaza para esconder las palabras
+ */
+	 public boolean sePuedeEsconder (){}
+
+/*
+ * Método que esconde las palabras
+ */
+	 public void esconderPalabra (){}
+/*
+ * Método que procesa las palabras
+ */
+	 public boolean procesarPalabra (){}
+/*
+ * Nos mostrara el nombre de las palabras que hay escondidas */
+	 public static void main (String[]args){}
+}
